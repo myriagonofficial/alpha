206d220081f46b9fa924d26ddacc9c8f50ff0b13
LeDetrogneur <44810668+LeDetrogneur@users.noreply.github.com>
2019-03-28 00:09:08 +0100

    Correctifs pour ajout de contenu

--- src/decks.js
+++ src/decks.js
@@ -1,14 +1,27 @@
 // deck = ère = ensemble de stories, mélangées
 
 export const decks = [
-    {
-        name: "(Re)naissance",
-        frame: "wood",
-        stories: ["lune", "tectonique", "vegetation_terrestre"],
-        jauges: [
-            { name: "Céleste", icon: "tree.png", level: 50 },
-            { name: "Terrestre ", icon: "stone.png", level: 50 },
-            { name: "Marine", icon: "animal.png", level: 50 }
-        ]
-    }
-]
+  {
+    name: "(Re)naissance",
+    frame: "wood",
+    stories: [
+      "lune",
+      "terres_arides",
+      "oceans_chauds",
+      "marecages",
+      "volcans",
+      "meteorites",
+      "oceans_geles",
+      "atmosphere_sombre",
+      "rayonnemets_dangereux",
+      "sel_de_mer",
+      "sol_glace",
+      "algues"
+    ],
+    jauges: [
+      { name: "Céleste", icon: "tree.png", level: 50 },
+      { name: "Terrestre", icon: "stone.png", level: 50 },
+      { name: "Marine", icon: "animal.png", level: 50 }
+    ]
+  }
+];
