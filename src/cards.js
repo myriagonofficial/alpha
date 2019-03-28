f060870e1cd38f29941e832151cd9a218ae8f294
LeDetrogneur <44810668+LeDetrogneur@users.noreply.github.com>
2019-03-28 14:00:01 +0100

    Pictos noirs
    Pour mieux voir la jauge

--- src/cards.js
+++ src/cards.js
@@ -195,34 +195,4 @@ export const cards = {
   }
 };
 
-/*
-{
-  name: "Carotte",
-    image: "carrot.jpg",
-      frame: "wood",
-        description: "Ceci est une carotte ",
-          yesLabel: "gauche",
-            noLabel: "droite",
-              yesEffects: [changeJauge("Animal", +10), changeJauge("Végétal", -20)],
-                noEffects: [changeJauge("Animal", -10), changeJauge("Végétal", +15)]
-},
-{
-  name: "Oeuf",
 
-    {
-      name: "Pêcheur",
-      image: "fisher.jpg",
-      frame: "wood",
-      description: "Ceci est un pêcheur",
-      yesEffects: [changeJauge("Animal", +10), changeJauge("Végétal", -20)],
-      noEffects: [changeJauge("Animal", -10), changeJauge("Végétal", +15)]
-    },
-    {
-      name: "Foudre",
-      image: "thunder.jpg",
-      frame: "wood",
-      description: "Ceci est un coup de foudre",
-      yesEffects: [changeJauge("Animal", +10), changeJauge("Végétal", -20)],
-      noEffects: [changeJauge("Animal", -10), changeJauge("Végétal", +15)]
-    }
-];*/
