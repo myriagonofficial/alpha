f07ab1010fa11650eef0760322949e830a6cc0c3
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2019-09-24 01:38:16 +0200

    fix preloader

--- src/audio.js
+++ src/audio.js
@@ -4,26 +4,25 @@ export const soundChannels = {}
 
 export const playSound = (key, channel) => {
     if (!(key in sounds)) return console.error(`Sound not found: ${key}`)
-    const sound = new Howl({ src: [sounds[key]] })
-    sound.play();
+    sounds[key].play();
     if (channel) {
         if (soundChannels[channel]) soundChannels[channel].stop() // stop previous sound in same channel
-        soundChannels[channel] = sound
+        soundChannels[channel] = sounds[key]
     }
 }
 
 export const playMusic = (key) => {
     if (!(key in musics)) return console.error(`Music not found: ${key}`)
     stopMusic();
-    const music = new Howl({ src: [musics[key]], loop: true })
-    music.play();
+    musics[key].play();
+    soundChannels.music = musics[key]
 }
 
 export const stopMusic = () => {
     if (soundChannels.music) soundChannels.music.stop()
 }
 
-export const sounds = {
+export const SOUNDS = {
     gui_card_on: 'assets/sound/gui_card_on.mp3',
     gui_card_off: 'assets/sound/gui_card_off.mp3',
     vox_gardien_01: 'assets/sound/vox_gardien_01.mp3',
@@ -33,9 +32,22 @@ export const sounds = {
     vox_gardien_05: 'assets/sound/vox_gardien_05.mp3'
 }
 
-export const musics = {
+export const MUSICS = {
     mus_menu: 'assets/sound/mus_menu_01.mp3',
     mus_gardien_01: 'assets/sound/mus_gardien_01.mp3',
     mus_gardien_02: 'assets/sound/mus_gardien_02.mp3',
     mus_gardien_03: 'assets/sound/mus_gardien_03.mp3'
 }
+
+export const sounds = Object.fromEntries(Object.entries(SOUNDS).map(([key, path]) => {
+    return [key, new Howl({
+        src: [path]
+    })];
+}))
+
+export const musics = Object.fromEntries(Object.entries(MUSICS).map(([key, path]) => {
+    return [key, new Howl({
+        src: [path],
+        loop: true
+    })];
+}))
--- src/preloader.js
+++ src/preloader.js
@@ -1,36 +1,41 @@
-import { sounds, musics } from "@/audio";
+import { SOUNDS, MUSICS } from "@/audio";
 import { cards } from "@/cards";
-import { deduplicate } from "@/utils"
+import { deduplicate } from "@/utils";
 
-const soundNames = [
-    ...Object.values(sounds),
-    ...Object.values(musics)
-]
+const audioFiles = [...Object.values(SOUNDS), ...Object.values(MUSICS)]
 
-const imageNames = [
-    ...deduplicate(Object.values(cards).map(card => `assets/cards/${card.image}`))
+const imageFiles = [
+    ...deduplicate(Object.values(cards).map(card => `assets/cards/${card.image}`)),
+    `assets/ALPHA_TITRE.png`,
+    `assets/MYRIAGON_LOGO.png`,
+    require(`./assets/ECRAN_INTRO.png`),
+    require(`./assets/ui/interface_fond_total.png`),
+    require(`./assets/ui/interface_barre_bas_fond.png`),
+    require(`./assets/ui/interface_barre_haut_fond.png`)
 ]
 
 export const preloadGame = (onProgress) => {
     return new Promise((resolve) => {
-        loadAssets(imageNames, soundNames, resolve, onProgress);
+        loadAssets(imageFiles, audioFiles, resolve, onProgress);
     })
 }
 
-export const loadAssets = function (imageNames, soundNames, callback, onProgress) {
+export const loadAssets = function (imageFiles, audioFiles, callback, onProgress) {
 
     // load multiple images and callback when ALL images have loaded
     let images = {},
         sounds = {},
-        assets = imageNames.concat(soundNames),
+        assets = imageFiles.concat(audioFiles),
         loaded = 0,
         loadedAssets = {},
         started = 0,
         MAX_CONCURRENT_DL = 10;
 
-    function onload(name) {
-        if (name in loadedAssets) return;
-        loadedAssets[name] = true;
+    console.log(assets)
+
+    function onload(src) {
+        if (src in loadedAssets) return;
+        loadedAssets[src] = true;
         loaded++;
         onProgress(loaded / assets.length * 100);
         /*if (this._loadingTimeout) {
@@ -45,26 +50,26 @@ export const loadAssets = function (imageNames, soundNames, callback, onProgress
     }
 
     function loadNext() {
-        var name = assets[started];
-        var type = started < imageNames.length ? "image" : "sound";
+        const src = assets[started];
+        const type = started < imageFiles.length ? "image" : "sound";
         started++;
 
         switch (type) {
             case "image":
-                images[name] = document.createElement('img');
-                images[name].addEventListener('load', () => onload(name));
-                images[name].addEventListener('error', () => onload(name));
-                images[name].src = "assets/" + name;
+                images[src] = document.createElement('img');
+                images[src].addEventListener('load', () => onload(src));
+                images[src].addEventListener('error', () => onload(src));
+                images[src].src = src;
                 break;
             case "sound":
-                sounds[name] = document.createElement('audio');
-                sounds[name].addEventListener('canplaythrough', () => onload(name), false);
-                sounds[name].addEventListener('load', () => onload(name), false);
-                sounds[name].addEventListener('error', () => onload(name), false);
-                //sounds[name]._loadingTimeout = setTimeout(onload, 5000); // too long to load, skip
-                sounds[name].src = 'assets/sound/' + name + ".mp3";
-                document.body.appendChild(sounds[name]);
-                sounds[name].load();
+                sounds[src] = document.createElement('audio');
+                sounds[src].addEventListener('canplaythrough', () => onload(src), false);
+                sounds[src].addEventListener('load', () => onload(src), false);
+                sounds[src].addEventListener('error', () => onload(src), false);
+                //sounds[src]._loadingTimeout = setTimeout(onload, 5000); // too long to load, skip
+                sounds[src].src = src;
+                document.body.appendChild(sounds[src]);
+                sounds[src].load();
                 break;
         }
     }
